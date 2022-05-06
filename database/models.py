2fc6601c19a0ae4f396dbff8d8933dc103ffb33a
Shivam-85 <103090492+Shivam-85@users.noreply.github.com>
2022-05-06 13:13:35 +0530

    Delete database directory

--- database/models.py
+++
@@ -1,9 +0,0 @@
-from .db import db
-
-
-class User(db.Document):
-    name = db.StringField(required=True, unique=True)
-    phone = db.StringField(required=True, unique=True)
-    age = db.IntField(required=True)
-    gender = db.StringField(required=True)
-
